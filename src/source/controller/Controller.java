be23b02a5965f2b9eeec3eff22734439e99b3c1d
varpihovsky <pakgpcoans@gmail.com>
2020-11-14 16:22:03 +0200

    Added basic servlet controller

---
+++ src/source/controller/Controller.java
@@ -0,0 +1,40 @@
+package controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet("/controller")
+public class Controller extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        process(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException{
+        process(request, response);
+    }
+
+    private void process(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException {
+        String page = request.getParameter("page");
+        String send = request.getParameter("send");
+
+        if(page.equals("/main.jsp") && send.equals("date")){
+            Date date = new Date();
+            request.setAttribute("date", date);
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(page);
+            dispatcher.forward(request, response);
+        }
+        else{
+            response.sendRedirect("error.jsp");
+        }
+    }
+}
