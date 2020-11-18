33da72595d33e821912490ca8e1e6690966f2c7b
varpihovsky <pakgpcoans@gmail.com>
2020-11-18 11:26:19 +0200

    Added checker class

---
+++ src/source/WebManager/security/Checker.java
@@ -0,0 +1,28 @@
+package WebManager.security;
+
+public class Checker {
+
+    public static boolean isContainsWrong(String str) {
+        if (str == null || str.contains("\'") || str.equals(""))
+            return true;
+        return false;
+    }
+
+    public static boolean checkLength(String str, int from, int to) {
+        int temp;
+        if (from > to) {
+            temp = from;
+            from = to;
+            to = temp;
+        }
+        if (str.length() >= from || str.length() <= to)
+            return true;
+        return false;
+    }
+
+    public static String pageReplace(String page) {
+        page = page.replace("/", "");
+        page = page.replace(".jsp", "");
+        return page;
+    }
+}
