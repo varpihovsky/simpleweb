2b24067f7b51eabc3d2eec82228a9c1135636d8b
varpihovsky <pakgpcoans@gmail.com>
2020-11-19 16:04:35 +0200

    Added some unit tests

---
+++ src/tests/WebManager/renderer/MainPageRendererTest.java
@@ -0,0 +1,64 @@
+package WebManager.renderer;
+
+
+import WebManager.HttpServletRequestImplemented;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import javax.servlet.http.*;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+
+public class MainPageRendererTest {
+    private static MainPageRenderer renderer;
+
+    @Mock
+    private static HttpServletRequestImplemented requestMock;
+
+    @Mock
+    private static HttpSession sessionMock;
+
+    @Before
+    public void before() {
+        renderer = new MainPageRenderer();
+        requestMock = mock(HttpServletRequestImplemented.class);
+        sessionMock = mock(HttpSession.class);
+
+        requestMock.names = new ArrayList<>();
+        requestMock.objects = new ArrayList<>();
+
+        Mockito.when(requestMock.getSession()).thenReturn(sessionMock);
+        Mockito.when(requestMock.getAttribute(Mockito.any())).thenCallRealMethod();
+        Mockito.doCallRealMethod().when(requestMock).setAttribute(Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    public void renderTestFirst() {
+        Mockito.when(sessionMock.getAttribute(eq("username"))).thenReturn("");
+        Mockito.when(sessionMock.getAttribute(eq("password"))).thenReturn("");
+
+        renderer.render(requestMock);
+
+        assertEquals("Not registered? <a href=\"/controller?page=register&send=redirect\">Register</a><br/>\n" +
+                        "    Registered? <a href=\"/controller?page=login&send=redirect\">Login</a>",
+                requestMock.getAttribute("render"));
+    }
+
+    @Test
+    public void renderTestSecond() {
+        Mockito.when(sessionMock.getAttribute(eq("username"))).thenReturn("username");
+        Mockito.when(sessionMock.getAttribute(eq("password"))).thenReturn("password");
+
+        renderer.render(requestMock);
+
+        assertEquals("Your username: username\n<br/>" +
+                        "    Your password: password\n<br/>" +
+                        "    <a href=\"/controller?page=profile&send=redirect\">Go to profile</a><br/>",
+                requestMock.getAttribute("render"));
+    }
+}
---
+++ src/tests/WebManager/renderer/RendererControllerTest.java
@@ -0,0 +1,41 @@
+package WebManager.renderer;
+
+import WebManager.HttpServletRequestImplemented;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertSame;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+
+public class RendererControllerTest {
+    private static RendererController controller;
+
+    @Mock
+    private static HttpServletRequestImplemented request;
+
+    @Before
+    public void before() {
+        controller = new RendererController();
+
+        request = mock(HttpServletRequestImplemented.class);
+    }
+
+    @Test
+    public void defineRendererTestFirst() {
+
+        Mockito.when(request.getParameter(eq("page"))).thenReturn(null);
+
+        assertSame(controller.defineRenderer(request).getClass(), NullRenderer.class);
+    }
+
+    @Test
+    public void defineRendererTestSecond() {
+        Mockito.when(request.getParameter(eq("page"))).thenReturn("main");
+
+        assertSame(controller.defineRenderer(request).getClass(), MainPageRenderer.class);
+    }
+}
