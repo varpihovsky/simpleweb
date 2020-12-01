ed199231a77b8c9c29cc4c0e5c667aa8cee7abae
varpihovsky <pakgpcoans@gmail.com>
2020-12-01 11:15:33 +0200

    SendExecutor executes ChangeSend now

--- src/source/WebManager/send/SendExecutor.java
+++ src/source/WebManager/send/SendExecutor.java
@@ -1,13 +1,21 @@
 package WebManager.send;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SendExecutor {
-    public static String execute(InterfaceSend send, HttpServletRequest request, HttpServletResponse response) {
+    public String execute(InterfaceSend send, HttpServletRequest request, HttpServletResponse response,
+                          ServletContext context) {
         if (send.getClass() == RedirectSend.class || send.getClass() == LoginSend.class ||
-                send.getClass() == LogoutSend.class || send.getClass() == ChangeSend.class)
+                send.getClass() == LogoutSend.class)
             return send.executeSend(request, response);
-        else return send.executeSend(request);
+        else if (send.getClass() == ChangeSend.class) {
+            Changer changer = (Changer) send;
+            changer.setServletContext(context);
+            InterfaceSend sender = (InterfaceSend) changer;
+            return sender.executeSend(request, response);
+        } else return send.executeSend(request);
     }
 }
