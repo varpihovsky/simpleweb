1bd7b08e6bb55747fc0d352af3575b7228c048ac
varpihovsky <pakgpcoans@gmail.com>
2020-12-01 23:24:50 +0200

    Added users page with find form.

---
+++ src/main/webapp/users.jsp
@@ -0,0 +1,40 @@
+<%@ page isELIgnored="false" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Users</title>
+    <link rel="stylesheet" href="css/basic.css">
+    <link rel="stylesheet" href="css/form.css">
+    <link rel="stylesheet" href="css/users.css">
+</head>
+<body>
+<div class="page-container">
+    <div class="navbar">
+        <div class="logo-text"><a href="/controller?page=main&send=redirect">echat</a></div>
+        <nav>
+            ${navbar}
+        </nav>
+    </div>
+    <div class="container">
+        <div class="form">
+            <form action="/controller">
+                <div class="find-form">
+                    <div class="input-form">
+                        <input type="text" name="user" value="">
+                    </div>
+                    <div class="input-form" id="find-button">
+                        <input type="submit" value="Find">
+                    </div>
+                    <input type="hidden" name="page" value="users">
+                    <input type="hidden" name="send" value="redirect">
+                </div>
+            </form>
+        </div>
+        <div class="user-grid">
+            ${render}
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- src/source/WebManager/send/DataBaseFactory.java
+++ src/source/WebManager/send/DataBaseFactory.java
@@ -10,9 +10,9 @@ import java.util.Arrays;
 
 
 public class DataBaseFactory {
-    private String url = "jdbc:mysql://localhost/simpleweb?useUnicode=true&serverTimezone=UTC";
-    private Connection connection;
-    private Statement statement;
+    private final String url = "jdbc:mysql://localhost/simpleweb?useUnicode=true&serverTimezone=UTC";
+    private final Connection connection;
+    private final Statement statement;
     private ResultSet resultSet;
 
     public DataBaseFactory() throws SQLException {
@@ -23,51 +23,50 @@ public class DataBaseFactory {
 
     public void addUser(String username, String email, String password)
             throws SQLException {
-        statement.executeUpdate("INSERT INTO simpleweb.user_data(USERNAME, PASS, EMAIL) VALUES (\'" + username +
-                "\', \'" + password + "\', \'" + email + "\');");
+        statement.executeUpdate("INSERT INTO simpleweb.user_data(USERNAME, PASS, EMAIL) VALUES ('" + username +
+                "', '" + password + "', '" + email + "');");
     }
 
     public void deleteUser(String username, String password, String email)
             throws SQLException {
-        statement.executeUpdate("DELETE FROM user_data WHERE USERNAME=\'"
-                + username + "\' AND PASS=\'" + password + "\' AND EMAIL=\'" + email + "\'");
-    }
-
-    public ArrayList<ArrayList<String>> getUserList() throws SQLException {
-        ArrayList<ArrayList<String>> userList = new ArrayList<>();
-        resultSet = statement.executeQuery("SELECT * FROM user_data");
-        for (int i = 0; resultSet.next(); i++) {
-            userList.add(new ArrayList<>());
-            userList.get(i).add(resultSet.getString(1));
-            userList.get(i).add(resultSet.getString(3));
-            userList.get(i).add(resultSet.getString(2));
-        }
-        return userList;
+        statement.executeUpdate("DELETE FROM user_data WHERE USERNAME='"
+                + username + "' AND PASS='" + password + "' AND EMAIL='" + email + "'");
     }
 
     public User getUserData(String username) throws SQLException {
-        resultSet = statement.executeQuery("SELECT * FROM user_data WHERE USERNAME=\'" + username + "\'");
+        resultSet = statement.executeQuery("SELECT * FROM user_data WHERE USERNAME='" + username + "'");
         resultSet.next();
         return new User(resultSet.getString(1), resultSet.getString(3),
                 resultSet.getString(2));
     }
 
     public boolean findUser(String username, String password) throws SQLException {
-        resultSet = statement.executeQuery("SELECT EXISTS(SELECT * FROM user_data WHERE USERNAME=\'" + username +
-                "\' AND PASS=\'" + password + "\')");
+        resultSet = statement.executeQuery("SELECT EXISTS(SELECT USERNAME FROM user_data WHERE USERNAME='" +
+                username + "' AND PASS='" + password + "')");
         resultSet.next();
         return resultSet.getBoolean(1);
     }
 
+    public ArrayList<String> userFind(String username, int num) throws SQLException {
+        resultSet = statement.executeQuery("SELECT USERNAME FROM user_data ORDER BY LEVENSHTEIN('" +
+                username + "', USERNAME) ASC LIMIT 0, " + num);
+
+        ArrayList<String> usernameArr = new ArrayList<>();
+        while (resultSet.next()) {
+            usernameArr.add(resultSet.getString(1));
+        }
+        return usernameArr;
+    }
+
     public boolean findAdmin(String username, String password) throws SQLException {
-        resultSet = statement.executeQuery("SELECT EXISTS(SELECT * FROM user_data WHERE USERNAME=\'" + username +
-                "\' AND PASS=\'" + password + "\' AND ISADMIN=\'yes\')");
+        resultSet = statement.executeQuery("SELECT EXISTS(SELECT * FROM user_data WHERE USERNAME='" + username +
+                "' AND PASS='" + password + "' AND ISADMIN='yes')");
         resultSet.next();
         return resultSet.getBoolean(1);
     }
 
     public ArrayList<Room> getRoomList(String username) throws SQLException {
-        resultSet = statement.executeQuery("SELECT rooms FROM user_data WHERE USERNAME=\'" + username + "\'");
+        resultSet = statement.executeQuery("SELECT rooms FROM user_data WHERE USERNAME='" + username + "'");
         resultSet.next();
         String roomString = resultSet.getString(1);
 
@@ -75,8 +74,8 @@ public class DataBaseFactory {
         ArrayList<String> descriptions = new ArrayList<>();
 
         for (int i = 0; i < names.size(); i++) {
-            resultSet = statement.executeQuery("SELECT description FROM room_data WHERE name=\'"
-                    + names.get(i) + "\'");
+            resultSet = statement.executeQuery("SELECT description FROM room_data WHERE name='"
+                    + names.get(i) + "'");
             resultSet.next();
             descriptions.add(resultSet.getString(1));
         }
@@ -90,30 +89,30 @@ public class DataBaseFactory {
     }
 
     public void changeEmail(String username, String email) throws SQLException {
-        statement.executeUpdate("UPDATE user_data SET EMAIL=\'" + email + "\' WHERE USERNAME=\'" + username + "\'");
+        statement.executeUpdate("UPDATE user_data SET EMAIL='" + email + "' WHERE USERNAME='" + username + "'");
     }
 
     public void changePassword(String username, String password) throws SQLException {
-        statement.executeUpdate("UPDATE user_data SET PASS=\'" + password + "\' WHERE USERNAME=\'" + username + "\'");
+        statement.executeUpdate("UPDATE user_data SET PASS='" + password + "' WHERE USERNAME='" + username + "'");
     }
 
     public void changeUsername(String oldUsername, String newUsername) throws SQLException {
-        statement.executeUpdate("UPDATE user_data SET USERNAME=\'" + newUsername + "\' WHERE USERNAME=\'" + oldUsername + "\'");
+        statement.executeUpdate("UPDATE user_data SET USERNAME='" + newUsername + "' WHERE USERNAME='" + oldUsername + "'");
     }
 
     public Room getRoom(String name) throws SQLException {
-        resultSet = statement.executeQuery("SELECT * FROM room_data WHERE name=\'" + name + "\'");
+        resultSet = statement.executeQuery("SELECT * FROM room_data WHERE name='" + name + "'");
         return new Room(resultSet.getString(1), resultSet.getString(2));
     }
 
     public void createRoom(String name, String description) throws SQLException {
-        statement.executeUpdate("INSERT INTO room_data(name, description, password) VALUES (\'" +
-                name + "\', \'" + description + "\', NULL");
+        statement.executeUpdate("INSERT INTO room_data(name, description, password) VALUES ('" +
+                name + "', '" + description + "', NULL");
     }
 
     public void createRoom(String name, String description, String isPrivate, String password)
             throws SQLException {
-        statement.executeUpdate("INSERT INTO room_data(name, description, password, isprivate) VALUES (\'" +
-                name + "\', \'" + description + "\', \'" + password + "\', \'" + isPrivate + "\'");
+        statement.executeUpdate("INSERT INTO room_data(name, description, password, isprivate) VALUES ('" +
+                name + "', '" + description + "', '" + password + "', '" + isPrivate + "'");
     }
 }
