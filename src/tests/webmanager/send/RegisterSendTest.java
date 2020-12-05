4a8a86d2345ac4e15b7493ee358ff5511ae26aec
varpihovsky <pakgpcoans@gmail.com>
2020-12-06 00:59:45 +0200

    RegisterSendTest created.

---
+++ src/tests/webmanager/send/RegisterSendTest.java
@@ -0,0 +1,55 @@
+package webmanager.send;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import test.implementations.HttpServletRequestImplemented;
+import test.implementations.HttpServletResponseImplemented;
+import test.implementations.HttpSessionImplemented;
+import webmanager.database.DatabaseController;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class RegisterSendTest {
+    RegisterSend send = new RegisterSend();
+
+    @Mock
+    HttpServletRequestImplemented requestMock;
+
+    @Mock
+    HttpServletResponseImplemented responseMock;
+
+    @Mock
+    DatabaseController controllerMock;
+
+    @Before
+    public void before() {
+        requestMock = mock(HttpServletRequestImplemented.class);
+        responseMock = mock(HttpServletResponseImplemented.class);
+        controllerMock = mock(DatabaseController.class);
+
+        Mockito.when(controllerMock.setOperation(Mockito.any(), Mockito.any())).thenReturn(controllerMock);
+    }
+
+    @Test
+    public void rightDataInputTest() {
+        Mockito.when(requestMock.getParameter("username")).thenReturn("Username");
+        Mockito.when(requestMock.getParameter("password")).thenReturn("Password");
+        Mockito.when(requestMock.getParameter("email")).thenReturn("someEmail@mail.com");
+        Mockito.when(requestMock.getParameter("page")).thenReturn("register");
+
+        assertEquals("register", send.executeSend(requestMock, responseMock, controllerMock));
+        Mockito.verify(controllerMock, Mockito.times(1)).execute();
+    }
+
+    @Test
+    public void wrongDataInputTest() {
+        Mockito.when(requestMock.getParameter(Mockito.any())).thenReturn(null);
+        Mockito.when(requestMock.getParameter("page")).thenReturn("register");
+
+        assertEquals("register", send.executeSend(requestMock, responseMock, controllerMock));
+        Mockito.verify(controllerMock, Mockito.times(0)).execute();
+    }
+}
