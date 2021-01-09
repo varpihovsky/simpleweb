d624ec0abbfa14069d72cd1f6a0b465966262643
varpihovsky <pakgpcoans@gmail.com>
2021-01-09 16:26:13 +0200

    DriverEnum moved to pool package.

---
+++ src/source/webmanager/database/pool/DriverEnum.java
@@ -0,0 +1,34 @@
+package webmanager.database.pool;
+
+import webmanager.util.Checker;
+
+import java.sql.Driver;
+import java.sql.SQLException;
+
+enum DriverEnum {
+    MYSQL {
+        {
+            try {
+                this.driver = new com.mysql.cj.jdbc.Driver();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    },
+    MARIADB {
+        {
+            this.driver = new org.mariadb.jdbc.Driver();
+        }
+    };
+    Driver driver;
+
+    static DriverEnum getInstance(String s) {
+        if (Checker.isContainsWrong(s))
+            return null;
+        return DriverEnum.valueOf(s.toUpperCase());
+    }
+
+    Driver getDriver() {
+        return driver;
+    }
+}
