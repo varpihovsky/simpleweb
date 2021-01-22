fdd5fff5bb0138c81d3ca99a604a40a5b6b0a98e
varpihovsky <pakgpcoans@gmail.com>
2021-01-22 16:35:02 +0200

    ConnectionPoolTest now runs with all tests together.

--- src/tests/webmanager/database/pool/ConnectionPoolTest.java
+++ src/tests/webmanager/database/pool/ConnectionPoolTest.java
@@ -1,13 +1,11 @@
 package webmanager.database.pool;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import webmanager.properties.PropertyManager;
@@ -17,18 +15,14 @@ import java.sql.Connection;
 import static org.junit.Assert.assertEquals;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
-//Warning!!! This test should be run independent from other tests!!!
-//TODO: do this test working with others tests
-@Ignore
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ConnectionPoolSupplier.class, PropertyManager.class})
-@PowerMockIgnore("org.mockito.*")
 public class ConnectionPoolTest {
     @Mock
     PropertyManager propertyManagerMock;
 
     @Mock
-    Connection connectionMock;
+    final static Connection connectionMock = Mockito.mock(Connection.class);
 
     @Before
     public void before() throws Exception {
@@ -46,14 +40,16 @@ public class ConnectionPoolTest {
     }
 
     @Test
+    public void testGetConnection() {
+        assertEquals(connectionMock, ConnectionPool.getConnection());
+        ConnectionPool.giveBack(connectionMock);
+    }
+
+    @Test
     public void testGiveBack() {
         Connection connection = ConnectionPool.getConnection();
         ConnectionPool.giveBack(connection);
         assertEquals(connectionMock, ConnectionPool.getConnection());
-    }
-
-    @Test
-    public void testGetConnection() {
-        assertEquals(connectionMock, ConnectionPool.getConnection());
+        ConnectionPool.giveBack(connection);
     }
 }
